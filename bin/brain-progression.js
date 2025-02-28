e37b281d78b87cf9b055e0bab42f2280c6e999f1
ilyavi <ilyavi@yandex.ru>
2025-02-28 11:14:10 +0700

    corrections

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,4 +4,4 @@ import brainProgression from '../src/games/progression_cli.js';
 
 const name1 = greetings();
 if (brainProgression() === true) console.log(`Congratulations, ${name1}!`);
-  else console.log(`Let's try again, ${name1}!`);
+else console.log(`Let's try again, ${name1}!`);
